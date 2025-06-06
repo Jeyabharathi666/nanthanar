22e0fada98bc0aa1c1d9aea2f94c22c6f6644e69
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2025-06-06 13:07:04 +0530

    Update moneycontrol.py

--- moneycontrol.py
+++ moneycontrol.py
@@ -12,7 +12,7 @@ def scrape_moneycontrol():
 
     try:
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True)
+            browser = p.chromium.launch(headless=False)
             page = browser.new_page()
 
             print("🌐 Navigating to the Analysts' Choice page...")
