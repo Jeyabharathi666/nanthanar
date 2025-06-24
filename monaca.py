c0c1bc3da58c077ad176da9a2e1b798847d3e861
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2025-06-24 16:39:14 +0530

    Update monaca.py

--- monaca.py
+++ monaca.py
@@ -107,7 +107,7 @@ def scrape_moneycontrol():
 
     try:
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=False)
+            browser = p.chromium.launch(headless=True)
             page = browser.new_page()
 
             print("🌐 Navigating to Analysts' Choice page...")
