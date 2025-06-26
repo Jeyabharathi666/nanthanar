c4a1b083f615270a3529067adc5121c016404cf9
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2025-06-26 07:35:30 +0530

    Update business_standard.py

--- business_standard.py
+++ business_standard.py
@@ -21,9 +21,9 @@ def scrape_business_standard():
             page = context.new_page()
 
             # GO TO PAGE without networkidle
-            page.goto(URL, timeout=60000)  # Allow up to 60s if slow network
+            page.goto(URL, timeout=150000)  # Allow up to 60s if slow network
             print("🌐 Page requested. Waiting fixed time for content...")
-            page.wait_for_timeout(10_000)  # 10 seconds fixed wait
+            page.wait_for_timeout(10000)  # 10 seconds fixed wait
 
             # SCRAPE
             trs = page.query_selector_all("table.cmpnydatatable_cmpnydatatable__Cnf6M tbody tr")
