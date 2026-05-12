10663ad995c358106d5fc703c09b364315b30f7c
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2026-05-12 14:30:06 +0530

    Update sector.py

--- sector.py
+++ sector.py
@@ -10,7 +10,7 @@ with open("creds.json", "w") as f:
 
 # === Google Sheets Setup ===
 SHEET_ID = "1VtgTb36SB65HtQQpjcagh4cxr7pDGcLzGpR9ScE4vdA"
-WORKSHEET_NAME = "nt"
+WORKSHEET_NAME = "FLIST"
 CREDENTIALS_FILE = "creds.json"
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
